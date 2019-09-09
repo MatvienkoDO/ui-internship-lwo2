68569150d1be8eee15d6a377f5a40689f49e9aa4
DMatvienko <dmatvienko@griddynamics.com>
2019-09-09 22:30:49 +0400

    adds solution of 9th task

---
+++ tasks/9_find-numbers/find-numbers.js
@@ -0,0 +1,9 @@
+export function getNumbers(str) {
+    const regexResult = str.match(/\d/g);
+
+    const numbers = (regexResult || [])
+        .map(Number)
+        .filter(n => !isNaN(n));
+
+    return numbers;
+}
