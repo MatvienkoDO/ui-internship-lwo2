4e606ccd6fbe5af3626963504993de0c0b6b4704
DMatvienko <dmatvienko@griddynamics.com>
2019-09-10 12:36:02 +0400

    fixes lint complaints for task 1st

--- tasks/1_golf-score/golf-score.js
+++ tasks/1_golf-score/golf-score.js
@@ -1,47 +1,47 @@
 export const GOLF_SCORES = {
-    HOLE_IN_ONE: 0,
-    EAGLE: 1,
-    BIRDIE: 2,
-    PAR: 3,
-    BOGEY: 4,
-    DOUBLE_BOGEY: 5,
-    GO_HOME: 6
-}
+  HOLE_IN_ONE: 0,
+  EAGLE: 1,
+  BIRDIE: 2,
+  PAR: 3,
+  BOGEY: 4,
+  DOUBLE_BOGEY: 5,
+  GO_HOME: 6,
+};
 
 export function golfScore(par, strokes) {
-    const {
-        HOLE_IN_ONE,
-        EAGLE,
-        BIRDIE,
-        PAR,
-        BOGEY,
-        DOUBLE_BOGEY,
-        GO_HOME
-    } = GOLF_SCORES;
+  const {
+    HOLE_IN_ONE,
+    EAGLE,
+    BIRDIE,
+    PAR,
+    BOGEY,
+    DOUBLE_BOGEY,
+    GO_HOME,
+  } = GOLF_SCORES;
 
-    if(strokes === 1) {
-        return HOLE_IN_ONE;
-    }
+  if (strokes === 1) {
+    return HOLE_IN_ONE;
+  }
 
-    if(strokes <= par - 2) {
-        return EAGLE;
-    }
+  if (strokes <= par - 2) {
+    return EAGLE;
+  }
 
-    if(strokes === par - 1) {
-        return BIRDIE;
-    }
+  if (strokes === par - 1) {
+    return BIRDIE;
+  }
 
-    if(strokes === par) {
-        return PAR;
-    }
+  if (strokes === par) {
+    return PAR;
+  }
 
-    if(strokes === par + 1) {
-        return BOGEY;
-    }
+  if (strokes === par + 1) {
+    return BOGEY;
+  }
 
-    if(strokes === par + 2) {
-        return DOUBLE_BOGEY;
-    }
+  if (strokes === par + 2) {
+    return DOUBLE_BOGEY;
+  }
 
-    return GO_HOME;
+  return GO_HOME;
 }
