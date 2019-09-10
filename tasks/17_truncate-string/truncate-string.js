2b789ea561fd224bd15a1e7f7021f0e2aab9be23
DMatvienko <dmatvienko@griddynamics.com>
2019-09-10 16:41:08 +0400

    fix: makes logic more right

--- tasks/17_truncate-string/truncate-string.js
+++ tasks/17_truncate-string/truncate-string.js
@@ -3,9 +3,9 @@ export function truncateString(str, maxLength) {
     return str;
   }
 
-  if (maxLength <= 3) {
-    maxLength += 3;
+  if (maxLength > 3) {
+    maxLength -= 3;
   }
 
-  return `${str.slice(0, maxLength - 3)}...`;
+  return `${str.slice(0, maxLength)}...`;
 }
