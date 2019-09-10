ec7fd0909484e413b6fecf1eefb8411caa9489d3
DMatvienko <dmatvienko@griddynamics.com>
2019-09-10 13:01:34 +0400

    style: resolves lint complaints

--- tasks/5_profile-lookup/profile-lookup.js
+++ tasks/5_profile-lookup/profile-lookup.js
@@ -1,45 +1,46 @@
 const contacts = [
-    {
-        "firstName": "Akira",
-        "lastName": "Laine",
-        "number": "0543236543",
-        "likes": ["Pizza", "Coding", "Brownie Points"]
-    },
-    {
-        "firstName": "Harry",
-        "lastName": "Potter",
-        "number": "0994372684",
-        "likes": ["Hogwarts", "Magic", "Hagrid"]
-    },
-    {
-        "firstName": "Sherlock",
-        "lastName": "Holmes",
-        "number": "0487345643",
-        "likes": ["Intriguing Cases", "Violin"]
-    },
-    {
-        "firstName": "Kristian",
-        "lastName": "Vos",
-        "number": "unknown",
-        "likes": ["Javascript", "Gaming", "Foxes"]
-    }
+  {
+    'firstName': 'Akira',
+    'lastName': 'Laine',
+    'number': '0543236543',
+    'likes': ['Pizza', 'Coding', 'Brownie Points'],
+  },
+  {
+    'firstName': 'Harry',
+    'lastName': 'Potter',
+    'number': '0994372684',
+    'likes': ['Hogwarts', 'Magic', 'Hagrid'],
+  },
+  {
+    'firstName': 'Sherlock',
+    'lastName': 'Holmes',
+    'number': '0487345643',
+    'likes': ['Intriguing Cases', 'Violin'],
+  },
+  {
+    'firstName': 'Kristian',
+    'lastName': 'Vos',
+    'number': 'unknown',
+    'likes': ['Javascript', 'Gaming', 'Foxes'],
+  },
 ];
 
 const errors = {
-    noSuchContact: 'No such contact',
-    noSuchProperty: 'No such property'
+  noSuchContact: 'No such contact',
+  noSuchProperty: 'No such property',
 };
 
 export function lookupProfile(firstName, prop) {
-    const contact = contacts.find(contact => contact && contact.firstName === firstName);
+  const contact = contacts
+      .find((contact) => contact && contact.firstName === firstName);
 
-    if(!contact) {
-        return errors.noSuchContact;
-    }
+  if (!contact) {
+    return errors.noSuchContact;
+  }
 
-    if(prop in contact === false) {
-        return errors.noSuchProperty;
-    }
+  if (prop in contact === false) {
+    return errors.noSuchProperty;
+  }
 
-    return contact[prop];
+  return contact[prop];
 }
