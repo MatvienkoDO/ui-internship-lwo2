c5c5211693071e0655eb7409636df3369863d649
DMatvienko <dmatvienko@griddynamics.com>
2019-09-12 16:30:03 +0400

    fix: task 28. Improves performance

--- tasks/28_vowel-count/vowel-count.js
+++ tasks/28_vowel-count/vowel-count.js
@@ -1,8 +1,11 @@
-const vowels = ['a', 'e', 'i', 'o', 'u'];
+// eslint-disable-next-line no-undef
+const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
 
 export function getVowelCount(text) {
   return text
       .split('')
-      .filter((c) => vowels.indexOf(c) !== -1)
-      .length;
+      .reduce(
+          (a, c) => vowels.has(c) ? a + 1 : a,
+          0
+      );
 }
