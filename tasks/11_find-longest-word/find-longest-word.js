701c453c91af6feae528e132e7262331f11923d7
DMatvienko <dmatvienko@griddynamics.com>
2019-09-12 15:07:57 +0400

    fix: uncomments second solution and deletes current

--- tasks/11_find-longest-word/find-longest-word.js
+++ tasks/11_find-longest-word/find-longest-word.js
@@ -1,20 +1,7 @@
-// solution with usage of all suggested functions
-export function findLongestWord(text) {
-  const max = text
-      .split(' ')
-      .map((word) => word.length)
-      .reduce((acc, curr) => {
-        return Math.max(acc, curr);
-      });
-
-  return max;
-}
-
-/* solution without reduce
 export function findLongestWord(text) {
   const lengths = text
       .split(' ')
       .map((word) => word.length);
 
   return Math.max(...lengths);
-}*/
+}
