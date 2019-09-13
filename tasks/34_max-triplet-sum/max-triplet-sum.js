c6212ea4c97476b55528a94a9ef46468d8424817
DMatvienko <dmatvienko@griddynamics.com>
2019-09-13 12:34:08 +0400

    fix: rewrites task 34

--- tasks/34_max-triplet-sum/max-triplet-sum.js
+++ tasks/34_max-triplet-sum/max-triplet-sum.js
@@ -1,24 +1,7 @@
 export function maxTripletSum(numbers, groupLength = 3) {
-  // eslint-disable-next-line no-undef
-  const uniqueNumbersIterator = new Set(numbers).values();
+  const uniqueNumbers = [...new Set(numbers).values()];
+  const sortedUnique = uniqueNumbers.sort((f, s) => s - f);
+  const group = sortedUnique.slice(0, groupLength);
 
-  const group = [];
-  let groupSum = 0;
-  for (let i = 0; i < groupLength; ++i) {
-    const element = uniqueNumbersIterator.next().value;
-    group.push(element);
-    groupSum += element;
-  }
-
-  let maxSum = groupSum;
-
-  for (const number of uniqueNumbersIterator) {
-    groupSum -= group.shift();
-    group.push(number);
-    groupSum += number;
-
-    maxSum = Math.max(maxSum, groupSum);
-  }
-
-  return maxSum;
+  return group.reduce((a, c) => a + c);
 }
