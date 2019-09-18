3127a5a902b1bd8a737f7e71fbe3dbac1d377cce
DMatvienko <dmatvienko@griddynamics.com>
2019-09-18 16:44:59 +0400

    fix: adds forgotten jsdoc to 70th

--- tasks/70_binary-decode/binary-decode.js
+++ tasks/70_binary-decode/binary-decode.js
@@ -1,3 +1,9 @@
+/**
+ * @description decodes string from binary representation
+ * @param {string} binary string in binary representation
+ * @return {string} decoded string
+ * @example '01010010 01100101' -> 'Re'
+ */
 export function binaryDecode(binary = '') {
   return binary
       .split(' ')
